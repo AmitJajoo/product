faab057162d4ffb5d7147de24f7631e2efbf50d3
Amitjajoo <amitkumarjajoo@gmail.com>
2024-11-06 08:34:15 +0530

    changes in product controller

--- src/main/java/com/amit/product/product/controller/ProductController.java
+++ src/main/java/com/amit/product/product/controller/ProductController.java
@@ -3,7 +3,7 @@ package com.amit.product.product.controller;
 
 import com.amit.product.product.dto.JsonResponse;
 import com.amit.product.product.entity.Product;
-import com.amit.product.product.service.ProductServiceImpl;
+import com.amit.product.product.service.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,43 +15,44 @@ import java.util.List;
 @RequestMapping("/products")
 public class ProductController {
 
-    private final ProductServiceImpl productServiceImpl;
+    private final ProductService productService;
 
-    public ProductController(ProductServiceImpl productServiceImpl) {
-        this.productServiceImpl = productServiceImpl;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
+
 
     @GetMapping
     public ResponseEntity<JsonResponse<List<Product>>> getAllProducts() {
-        List<Product> products = productServiceImpl.getAllProducts();
+        List<Product> products = productService.getAllProducts();
         JsonResponse<List<Product>> response = new JsonResponse<>(products, "Products retrieved successfully",true, HttpStatus.OK.value());
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<JsonResponse<Product>> getProductById(@PathVariable Long id) {
-        Product product = productServiceImpl.getProductById(id);
+        Product product = productService.getProductById(id);
         JsonResponse<Product> response = new JsonResponse<>(product, "Product retrieved successfully",true,  HttpStatus.OK.value());
         return ResponseEntity.ok(response);
     }
 
     @PostMapping
     public ResponseEntity<JsonResponse<Product>> createProduct(@Valid @RequestBody Product product) {
-        Product createdProduct = productServiceImpl.createProduct(product);
+        Product createdProduct = productService.createProduct(product);
         JsonResponse<Product> response = new JsonResponse<>(createdProduct, "Product created successfully", true, HttpStatus.CREATED.value());
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<JsonResponse<Product>> updateProduct(@PathVariable Long id, @Valid @RequestBody Product product) {
-        Product updatedProduct = productServiceImpl.updateProduct(id, product);
+        Product updatedProduct = productService.updateProduct(id, product);
         JsonResponse<Product> response = new JsonResponse<>(product, "Product updated successfully",true,  HttpStatus.OK.value());
         return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<JsonResponse<Void>> deleteProduct(@PathVariable Long id) {
-        productServiceImpl.deleteProduct(id);
+        productService.deleteProduct(id);
         JsonResponse<Void> response = new JsonResponse<>(null, "Product deleted successfully", true, HttpStatus.NO_CONTENT.value());
         return new ResponseEntity<>(response,HttpStatus.NO_CONTENT);
     }
